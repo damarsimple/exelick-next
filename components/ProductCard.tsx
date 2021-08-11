cacbdbbeffaa8eac1734bf7d65e9855e64f5b33c
Damar Albaribin <damaralbaribin@gmail.com>
2021-08-11 10:08:24 +0700

    feat : plus minus button

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -20,6 +20,12 @@ export default function ProductCard() {
       />
       <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
         <h1 className="text-gray-200 font-bold text-xl">Rp 10.000</h1>
+        <button className="px-3 py-1 bg-gray-200 text-md text-gray-900 font-semibold rounded">
+          +
+        </button>
+        <button className="px-3 py-1 bg-gray-200 text-md text-gray-900 font-semibold rounded">
+          -
+        </button>
         <button className="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded">
           Add to card
         </button>
