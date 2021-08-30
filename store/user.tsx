f63b10833e01095cb6501a695d203c7dab2f83d0
Damar Albaribin <damaralbaribin@gmail.com>
2021-08-30 13:49:56 +0700

    fix : user

--- store/user.tsx
+++ store/user.tsx
@@ -1,12 +1,30 @@
 import create from "zustand";
 import { User } from "../types/type";
+import { persist } from "zustand/middleware";
 
 interface UserStore {
   user: User | null | undefined;
   setUser: (e: User | null | undefined) => void;
 }
 
-export const useUserStore = create<UserStore>((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-}));
+// export const useUserStore = persist<UserStore>(
+//   create((set) => ({
+//     user: null,
+//     setUser: (user) => set({ user }),
+//   })),
+//   {
+//     name: "user-storage",
+//   }
+// );
+
+export const useUserStore = create<UserStore>(
+  persist(
+    (set, get) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+    }),
+    {
+      name: "user-storage", // unique name
+    }
+  )
+);
